13e68418daa9d45386a1dcd413472a1c9c3ea165
nishinoyama <begoingto1414@gmail.com>
2019-09-26 13:51:49 +0900

    add:send and reply

---
+++ index.js
@@ -0,0 +1,12 @@
+
+const {ipcRenderer} = require('electron');
+
+let flag;
+
+window.onload = function(){
+}
+
+function sendAndReply( mes ){
+    const result = ipcRenderer.sendSync( 'message', mes );
+    alert(result);
+}
--- main.js
+++ main.js
@@ -3,18 +3,31 @@ const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
 let mainWindow = null;
+
+let flag = 114514;
+
+function mo(){
+    return flag;
+}
+
 app.on( 'ready', () => {
-    
-    mainWindow = new BrowserWindow({width: 400, height:300 });
 
+    mainWindow = new BrowserWindow({
+        width:600, height:500,
+        webPreferences:{
+            nodeIntegration: true
+        }
+    });
     mainWindow.loadURL('file://' + __dirname + '/index.html' );
-
     mainWindow.webContents.openDevTools();
-
     mainWindow.on( 'closed', function() {
         mainWindow = null;
     });
 
 });
 
-
+const { ipcMain } = require('electron');
+ipcMain.on('message', (event, arg) => {
+    console.log(arg);
+    event.returnValue = arg;
+});
